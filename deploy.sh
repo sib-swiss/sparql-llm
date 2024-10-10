7a9d6aa8a9aefcbaefc32c526c2d18b528baa1e4
Vincent Emonet <vincent.emonet@gmail.com>
2024-10-10 09:37:44 +0200

    improve docker setup, add 1 test

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,20 @@
 if [ "$1" = "--build" ]; then
     echo "📦️ Re-building"
     ssh expasychat 'sudo -u podman bash -c "cd /var/containers/podman/sparql-llm ; git pull ; podman-compose up --force-recreate --build -d"'
+
+elif [ "$1" = "--clean" ]; then
+    echo "🧹 Cleaning up the vector database"
+    ssh expasychat 'sudo -u podman bash -c "cd /var/containers/podman/sparql-llm ; git pull ; rm -rf data/qdrant ; podman-compose up --force-recreate -d"'
+
 elif [ "$1" = "--logs" ]; then
     ssh expasychat 'sudo -u podman bash -c "cd /var/containers/podman/sparql-llm ; podman-compose logs api"'
+
 elif [ "$1" = "--likes" ]; then
     mkdir -p data/prod
     scp expasychat:/var/containers/podman/sparql-llm/data/logs/likes.jsonl ./data/prod/
     scp expasychat:/var/containers/podman/sparql-llm/data/logs/dislikes.jsonl ./data/prod/
     scp expasychat:/var/containers/podman/sparql-llm/data/logs/user_questions.log ./data/prod/
+
 else
     ssh expasychat 'sudo -u podman bash -c "cd /var/containers/podman/sparql-llm ; git pull ; podman-compose up --force-recreate -d"'
 fi
