d858e574f854a0b259cd0c743c61261ca94f4fd1
Vincent Emonet <vincent.emonet@gmail.com>
2024-10-28 10:34:33 +0100

    fix global imports and bump to 0.0.3

--- src/sparql_llm/__init__.py
+++ src/sparql_llm/__init__.py
@@ -1,8 +1,8 @@
 """Utilities to improve LLMs capabilities when working with SPARQL and RDF."""
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
-from .void_to_shex import get_shex_dict_from_void
+from .void_to_shex import get_shex_dict_from_void, get_shex_from_void
 from .validate_sparql import validate_sparql_with_void
 from .sparql_examples_loader import SparqlExamplesLoader
 from .sparql_void_shapes_loader import SparqlVoidShapesLoader
