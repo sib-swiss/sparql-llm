ecfd85df4c4c4c41693cf7efad907c01034d9d05
Panayiotis Smeros <panayiotis.smeros@sib.swiss>
2025-07-17 18:47:19 +0300

    Added datatypes for predicates in class descriptions

--- src/expasy-agent/tests/text2sparql/endpoint_schema.py
+++ src/expasy-agent/tests/text2sparql/endpoint_schema.py
@@ -9,13 +9,15 @@ tqdm.pandas()
 logger = logging.getLogger("sparql_llm")
 logger.setLevel(logging.INFO)
 
+TOP_N_PREDICATES = 20
+TOP_CLASSES_PERCENTILE = .90
+
 CLASS_QUERY = """
 SELECT ?class (COUNT(?class) AS ?count)
 WHERE { 
     [] a ?class . 
 }
 GROUP BY ?class
-ORDER BY DESC(?count)
 """
 
 PREDICATE_QUERY = """
@@ -26,6 +28,7 @@ WHERE {{
 }}
 GROUP BY ?predicate
 ORDER BY DESC(?count)
+LIMIT {limit}
 """
 
 DATATYPE_QUERY = """
@@ -33,6 +36,7 @@ SELECT ?range
 WHERE {{
     [] a <{class_name}> ;
          <{predicate_name}> ?o .
+         FILTER (isLiteral(?o)) .
          BIND(datatype(?o) AS ?range)
 }}
 GROUP BY ?range
@@ -47,7 +51,7 @@ EXCLUDED_PREDICATES = [
     'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
 ]
 
-def get_class_info(endpoint_url: str, probability_threshold: float = 0) -> pd.DataFrame:
+def get_class_info(endpoint_url: str) -> pd.DataFrame:
     """Get class information from the SPARQL endpoint."""
 
     logger.info(f'Fetching class information from {endpoint_url}...')
@@ -55,24 +59,27 @@ def get_class_info(endpoint_url: str, probability_threshold: float = 0) -> pd.Da
     classes = pd.DataFrame(classes).map(lambda x: x['value']).assign(count=lambda df: df['count'].astype(int))
     classes = classes[~classes['class'].isin(EXCLUDED_CLASSES)]
     classes['name'] = classes['class'].apply(lambda c: re.sub(r'(?<!^)(?=[A-Z])', ' ', c.split('/')[-1].split('#')[-1]))
-    classes['probability'] = round(classes['count'] / classes['count'].sum(), 3)
-    classes = classes[classes['probability'] > probability_threshold].sort_values(by='probability', ascending=False).reset_index(drop=True)
+
+    num_classes = len(classes) 
+    count_threshold = classes['count'].quantile(TOP_CLASSES_PERCENTILE)
+    classes = classes[classes['count'] >= count_threshold].sort_values(by='count', ascending=False).reset_index(drop=True)
+    logger.info(f'Keeping {len(classes)}/{num_classes} most frequent classes.')
+
     logger.info(f'Fetching predicate information from {endpoint_url}...')
-    classes['predicates'] = classes['class'].progress_apply(lambda c: get_class_predicates(endpoint_url=endpoint_url, class_name=c, probability_threshold=probability_threshold))
+    classes['predicates'] = classes['class'].progress_apply(lambda c: get_class_predicates(endpoint_url=endpoint_url, class_name=c))
     return classes
 
 
-def get_class_predicates(endpoint_url: str, class_name: str, probability_threshold: float = 0) -> dict[str, float]:
-    """Get the predicates for a given class from the SPARQL endpoint."""
+def get_class_predicates(endpoint_url: str, class_name: str) -> dict[str, str]:
+    """Get the predicates and their ranges for a given class from the SPARQL endpoint."""
 
-    predicates = query_sparql(PREDICATE_QUERY.format(class_name=class_name), endpoint_url=endpoint_url)['results']['bindings']
+    predicates = query_sparql(PREDICATE_QUERY.format(class_name=class_name, limit=TOP_N_PREDICATES), endpoint_url=endpoint_url)['results']['bindings']
     predicates = pd.DataFrame(predicates).map(lambda x: x['value']).assign(count=lambda df: df['count'].astype(int))
     predicates = predicates[~predicates['predicate'].isin(EXCLUDED_PREDICATES)]
-    predicates['probability'] = round(predicates['count'] / predicates['count'].sum(), 3)
-    predicates = predicates[predicates['probability'] > probability_threshold].sort_values(by='probability', ascending=False).reset_index(drop=True)
-
-    predicates['range'] = predicates['predicate'].apply(lambda p: query_sparql(DATATYPE_QUERY.format(class_name=class_name, predicate_name=p), endpoint_url=endpoint_url)['results']['bindings'][0])
-    predicates['range'] = predicates['range'].apply(lambda r: r['range']['value'] if 'range' in r.keys() else None)
+    
+    predicates['range'] = predicates['predicate'].apply(lambda p: (query_sparql(DATATYPE_QUERY.format(class_name=class_name, predicate_name=p), endpoint_url=endpoint_url)['results']['bindings'] or [{}])[0])
+    predicates['range'] = predicates['range'].apply(lambda r: r['range']['value'] if 'range' in r.keys() else pd.NA)
+    predicates = predicates.dropna(subset=['range'])
 
     return predicates.set_index('predicate')['range'].to_dict()
 
@@ -80,7 +87,6 @@ def get_class_predicates(endpoint_url: str, class_name: str, probability_thresho
 if __name__ == "__main__":
     endpoint_url = 'http://localhost:8890/sparql/'
     start_time = time.time()
-    # class_info = get_class_info(endpoint_url=endpoint_url)
-    l = get_class_predicates(endpoint_url=endpoint_url, class_name='http://xmlns.com/foaf/0.1/Person', probability_threshold=0.01)
+    class_info = get_class_info(endpoint_url=endpoint_url)
     elapsed_time = time.time() - start_time
     logger.info(f"Total execution time: {elapsed_time / 60:.2f} minutes")
