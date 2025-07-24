17358140353e1dde1a4dd2c6fd635f2f7b687815
Panayiotis Smeros <panayiotis.smeros@sib.swiss>
2025-07-24 17:16:54 +0200

    Evaluate all languages

--- src/expasy-agent/tests/text2sparql/evaluate.sh
+++ src/expasy-agent/tests/text2sparql/evaluate.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # This script runs Text2SPARQL evaluation tests against a local API.
-
+# Run Command: $ uv run src/expasy-agent/tests/text2sparql/evaluate.sh
 
 API_URL='http://localhost:8765'
 ENDPOINT_URL='http://localhost:8890/sparql'
 TIMESTAMP=$(date +"%Y%m%d_%H%M")
 QUESTIONS="data/benchmarks/Text2SPARQL/queries/questions_db25.yaml"
 QUERIES="data/benchmarks/${TIMESTAMP}_Text2SPARQL_queries.json"
-RESULTS="data/benchmarks/${TIMESTAMP}_Text2SPARQL_results.json"
+RESULTS_EN="data/benchmarks/${TIMESTAMP}_EN_Text2SPARQL_results.json"
+RESULTS_ES="data/benchmarks/${TIMESTAMP}_ES_Text2SPARQL_results.json"
+RESULTS_FULL="data/benchmarks/${TIMESTAMP}_FULL_Text2SPARQL_results.json"
 CACHE="data/benchmarks/${TIMESTAMP}_Text2SPARQL_responses.db"
 
 
@@ -18,10 +20,14 @@ uv pip install text2sparql-client
 text2sparql ask --answers-db "$CACHE" -o "$QUERIES" "$QUESTIONS" "$API_URL"
 
 # Evaluate the queries against the SPARQL endpoint and save results
-text2sparql evaluate -e "$ENDPOINT_URL" -o "$RESULTS" expasy "$QUESTIONS" "$QUERIES"
+text2sparql evaluate -l "['en']" -e "$ENDPOINT_URL" -o "$RESULTS_EN" expasy "$QUESTIONS" "$QUERIES"
+text2sparql evaluate -l "['es']" -e "$ENDPOINT_URL" -o "$RESULTS_ES" expasy "$QUESTIONS" "$QUERIES"
+text2sparql evaluate -l "['en','es']" -e "$ENDPOINT_URL" -o "$RESULTS_FULL" expasy "$QUESTIONS" "$QUERIES"
 
 # Clean up temporary files
 rm -f "$QUERIES" "$CACHE"
 
 # Print the results
-echo "Average F1 score: $(jq -r '.average.set_F' "$RESULTS")"
+echo "Average F1 score (EN): $(jq -r '.average.set_F' "$RESULTS_EN")"
+echo "Average F1 score (ES): $(jq -r '.average.set_F' "$RESULTS_ES")"
+echo "Average F1 score (FULL): $(jq -r '.average.set_F' "$RESULTS_FULL")"
