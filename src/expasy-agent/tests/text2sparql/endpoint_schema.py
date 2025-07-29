86c959738b82a85753950b1f8a7f24100152237f
Panayiotis Smeros <panayiotis.smeros@sib.swiss>
2025-07-29 11:46:03 +0200

    Added name graph support for schema queries

--- src/expasy-agent/tests/text2sparql/endpoint_schema.py
+++ src/expasy-agent/tests/text2sparql/endpoint_schema.py
@@ -9,20 +9,18 @@ from tqdm import tqdm
 logger = logging.getLogger("sparql_llm")
 logger.setLevel(logging.INFO)
 
-TOP_N_PREDICATES = 20
-TOP_CLASSES_PERCENTILE = .90
-TOP_N_RANGE_CLASSES = 5
-
 CLASS_QUERY = """
 SELECT ?class (COUNT(?class) AS ?count)
-WHERE { 
+FROM <{graph}>
+WHERE {{
     ?s a ?class .
-}
+}}
 GROUP BY ?class
 """
 
 PREDICATE_QUERY = """
 SELECT ?predicate (COUNT(?predicate) AS ?count)
+FROM <{graph}>
 WHERE {{
     ?s a <{class_name}> ;
         ?predicate ?o .
@@ -34,10 +32,11 @@ LIMIT {limit}
 
 RANGE_DATATYPE_QUERY = """
 SELECT ?range
+FROM <{graph}>
 WHERE {{
     ?s a <{class_name}> ;
-         <{predicate_name}> ?o .
-         FILTER (isLiteral(?o)) .
+        <{predicate_name}> ?o .
+    FILTER (isLiteral(?o)) .
 }}
 GROUP BY (datatype(?o) AS ?range)
 ORDER BY DESC(COUNT(?range))
@@ -46,11 +45,12 @@ LIMIT 1
 
 RANGE_CLASS_QUERY = """
 SELECT ?range
+FROM <{graph}>
 WHERE {{
     ?s a <{class_name}> ;
         <{predicate_name}> ?o .
-        ?o a ?range . 
-        FILTER (?range NOT IN (<http://www.w3.org/2002/07/owl#Thing>)) .
+    ?o a ?range .
+    FILTER (?range NOT IN (<http://www.w3.org/2002/07/owl#Thing>)) .
 }}
 GROUP BY ?range
 ORDER BY DESC(COUNT(?range))
@@ -58,56 +58,52 @@ LIMIT {limit}
 """
 
 
-EXCLUDED_CLASSES = [
-    'http://www.w3.org/2002/07/owl#Thing',
-    ]
-EXCLUDED_PREDICATES = [
-]
-
-def get_class_info(endpoint_url: str, max_workers: int = 4) -> pd.DataFrame:
+def get_class_info(endpoint_url: str, graph: str, limit_queries: dict[str, float], max_workers: int) -> pd.DataFrame:
     """Get class information from the SPARQL endpoint."""
 
     logger.info(f'Fetching class information from {endpoint_url}...')
-    classes = query_sparql(CLASS_QUERY, endpoint_url=endpoint_url)['results']['bindings']
+    classes = query_sparql(CLASS_QUERY.format(graph=graph), endpoint_url=endpoint_url)['results']['bindings']
     classes = pd.DataFrame(classes).map(lambda x: x['value']).assign(count=lambda df: df['count'].astype(int))
-    classes = classes[~classes['class'].isin(EXCLUDED_CLASSES)]
     classes['name'] = classes['class'].apply(lambda c: re.sub(r'(?<!^)(?=[A-Z])', ' ', c.split('/')[-1].split('#')[-1]))
 
     # Filter classes based on the count of instances
     num_classes = len(classes) 
-    count_threshold = classes['count'].quantile(TOP_CLASSES_PERCENTILE)
+    count_threshold = classes['count'].quantile(limit_queries['top_classes_percentile'])
     classes = classes[classes['count'] >= count_threshold].sort_values(by='count', ascending=False).reset_index(drop=True)
     logger.info(f'Keeping {len(classes)}/{num_classes} most frequent classes.')
 
     #Fetch range information for predicates of each class in parallel
     logger.info(f'Fetching predicate information from {endpoint_url}...')
     # classes['predicates'] = classes['class'].apply(lambda c: get_class_predicates(endpoint_url=endpoint_url, class_name=c))
-    classes['predicates'] = Parallel(n_jobs=max_workers)(delayed(get_class_predicates)(endpoint_url=endpoint_url, class_name=c) for c in tqdm(classes['class'].tolist(), total=len(classes)))
+    classes['predicates'] = Parallel(n_jobs=max_workers)(delayed(get_class_predicates)(endpoint_url=endpoint_url, graph=graph, limit_queries=limit_queries, class_name=c) for c in tqdm(classes['class'].tolist(), total=len(classes)))
 
     return classes
 
 
-def get_class_predicates(endpoint_url: str, class_name: str) -> dict[str, str]:
+def get_class_predicates(endpoint_url: str, graph: str, limit_queries: dict[str, float], class_name: str) -> dict[str, str]:
     """Get the predicates and their ranges for a given class from the SPARQL endpoint."""
 
     # Fetch predicates for the class
-    predicates = query_sparql(PREDICATE_QUERY.format(class_name=class_name, limit=TOP_N_PREDICATES), endpoint_url=endpoint_url)['results']['bindings']
+    predicates = query_sparql(PREDICATE_QUERY.format(graph=graph, class_name=class_name, limit=limit_queries['top_n_predicates']), endpoint_url=endpoint_url)['results']['bindings']
     predicates = pd.DataFrame(predicates).map(lambda x: x['value']).assign(count=lambda df: df['count'].astype(int))
-    predicates = predicates[~predicates['predicate'].isin(EXCLUDED_PREDICATES)]
     
     # Fetch datatypes range
-    predicates['range'] = predicates['predicate'].apply(lambda p: (query_sparql(RANGE_DATATYPE_QUERY.format(class_name=class_name, predicate_name=p), endpoint_url=endpoint_url)['results']['bindings'] or [{}])[0]).apply(lambda r: r['range']['value'] if 'range' in r.keys() else pd.NA)
+    predicates['range'] = predicates['predicate'].apply(lambda p: (query_sparql(RANGE_DATATYPE_QUERY.format(graph=graph, class_name=class_name, predicate_name=p), endpoint_url=endpoint_url)['results']['bindings'] or [{}])[0]).apply(lambda r: r['range']['value'] if 'range' in r.keys() else pd.NA)
     #Fetch classes range
-    predicates.loc[predicates['range'].isna(), 'range'] = predicates[predicates['range'].isna()]['predicate'].apply(lambda p: (query_sparql(RANGE_CLASS_QUERY.format(class_name=class_name, predicate_name=p, limit=TOP_N_RANGE_CLASSES), endpoint_url=endpoint_url)['results']['bindings'] or [{}])).apply(lambda l: [r['range']['value'] for r in l]  if l != [{}] else pd.NA)
+    predicates.loc[predicates['range'].isna(), 'range'] = predicates[predicates['range'].isna()]['predicate'].apply(lambda p: (query_sparql(RANGE_CLASS_QUERY.format(graph=graph, class_name=class_name, predicate_name=p, limit=limit_queries['top_n_range_classes']), endpoint_url=endpoint_url)['results']['bindings'] or [{}])).apply(lambda l: [r['range']['value'] for r in l]  if l != [{}] else pd.NA)
     predicates.loc[predicates['range'].isna(), 'range'] = ''
 
     return predicates.set_index('predicate')['range'].to_dict()
 
 
 if __name__ == "__main__":
-    endpoint_url = 'http://localhost:8890/sparql/'
-    max_workers = 4
     start_time = time.time()
-    class_info = get_class_info(endpoint_url=endpoint_url, max_workers=max_workers)
+    class_info = get_class_info(endpoint_url = 'http://localhost:8890/sparql/', 
+                                graph = 'https://text2sparql.aksw.org/2025/corporate/',
+                                limit_queries = {'top_classes_percentile' : .90,
+                                                 'top_n_predicates' : 20,
+                                                 'top_n_range_classes' : 5,
+                                                },
+                                max_workers = 4)
     elapsed_time = time.time() - start_time
     logger.info(f"Total execution time: {elapsed_time / 60:.2f} minutes")
