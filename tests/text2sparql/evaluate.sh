d22c199cd91a640089ccb891f8dbda401f37b0cd
Panayiotis Smeros <panayiotis.smeros@sib.swiss>
2025-10-07 17:40:34 +0200

    Removed overall results from script

--- tests/text2sparql/evaluate.sh
+++ tests/text2sparql/evaluate.sh
@@ -10,7 +10,6 @@ QUESTIONS_CK="data/benchmarks/Text2SPARQL/queries/questions_ck25.yaml"
 QUERIES="data/benchmarks/${TIMESTAMP}_Text2SPARQL_queries.json"
 RESULTS_EN="data/benchmarks/${TIMESTAMP}_EN_Text2SPARQL_results.json"
 RESULTS_ES="data/benchmarks/${TIMESTAMP}_ES_Text2SPARQL_results.json"
-RESULTS_FULL="data/benchmarks/${TIMESTAMP}_FULL_Text2SPARQL_results.json"
 RESULTS_CK="data/benchmarks/${TIMESTAMP}_CK_Text2SPARQL_results.json"
 CACHE="data/benchmarks/${TIMESTAMP}_Text2SPARQL_responses.db"
 
@@ -20,7 +19,6 @@ uv pip install text2sparql-client
 
 #if the argument contains "ck" then use the ck questions file
 if [[ "$1" == *"ck"* ]]; then
-
     # Ask questions from the questions file on your endpoint
     uv run text2sparql ask --answers-db "$CACHE" -o "$QUERIES" "$QUESTIONS_CK" "$API_URL"
 
@@ -29,14 +27,12 @@ if [[ "$1" == *"ck"* ]]; then
 fi
 
 if [[ "$1" == *"db"* ]]; then
-
     # Ask questions from the questions file on your endpoint
     uv run text2sparql ask --answers-db "$CACHE" -o "$QUERIES" "$QUESTIONS_DB" "$API_URL"
 
     # Evaluate the queries against the SPARQL endpoint and save results
     uv run text2sparql evaluate -l "['en']" -e "$ENDPOINT_URL" -o "$RESULTS_EN" ExpasyGPT "$QUESTIONS_DB" "$QUERIES"
     uv run text2sparql evaluate -l "['es']" -e "$ENDPOINT_URL" -o "$RESULTS_ES" ExpasyGPT "$QUESTIONS_DB" "$QUERIES"
-    uv run text2sparql evaluate -l "['en','es']" -e "$ENDPOINT_URL" -o "$RESULTS_FULL" ExpasyGPT "$QUESTIONS_DB" "$QUERIES"
 fi
 
 # Print the results
@@ -44,8 +40,6 @@ if [[ "$1" == *"ck"* ]]; then
     echo "Average F1 score (CK): $(jq -r '.average.set_F' "$RESULTS_CK")"
 fi
 if [[ "$1" == *"db"* ]]; then
-
     echo "Average F1 score (EN): $(jq -r '.average.set_F' "$RESULTS_EN")"
     echo "Average F1 score (ES): $(jq -r '.average.set_F' "$RESULTS_ES")"
-    echo "Average F1 score (FULL): $(jq -r '.average.set_F' "$RESULTS_FULL")"
 fi
